832c5c06e4e8cb926b9efa07c224d9caf1065761
bjethro <@github.com>
2019-05-01 21:35:08 -1000

    XC

--- ziplist.js
+++ ziplist.js
@@ -1,18 +1,17 @@
-const testList1 = ['a', 'b', 'c'];
-const testList2 = [1, 2, 3];
-
 function zipList(list1, list2) {
-  const result = [];
+  const res = [];
   for (let i = 0; i < list1.length; i++) {
-    result.push(list1[i], list2[i]);
+    res.push(list1[i]);
+    res.push(list2[i]);
   }
-  return result;
+  return res;
 }
 
-console.log(zipList(testList1, testList2));
 
-function zipListTheSimpleAway(list1, list2) {
+function zipListTheSimpleWay(list1, list2) {
   return _.flatten(_.zip(list1, list2));
 }
 
+
+console.log(zipList(testList1, testList2));
 console.log(zipListTheSimpleAway(testList1, testList2));
